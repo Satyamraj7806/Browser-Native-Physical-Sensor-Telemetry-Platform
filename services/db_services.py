89b3da643eba23652d59b62330e573e45afd08a9
Satyamraj7806 <0786satyamraj@gmail.com>
2026-05-01 14:44:31 +0530

    add configuration and project dependenciesD

---
+++ services/db_services.py
@@ -0,0 +1,12 @@
+class DBService:
+    """Database service for logging sensor data and events"""
+
+    def log_sensor(self, device_id: str, sensor_type: str, data: dict):
+        """Log sensor data to database"""
+        # TODO: Implement database logging for sensor data
+        pass
+
+    def log_event(self, device_id: str, event_type: str, data: dict):
+        """Log event to database"""
+        # TODO: Implement database logging for events
+        pass
